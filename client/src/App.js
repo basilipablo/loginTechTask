6f95b10a61dffd846af81302dbed05211ddb97a7
Pablo <basilipablo@gmail.com>
2021-07-07 16:31:09 -0300

    last functionality is taking me some time. The socket

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,22 @@ import RegisterBox from './components/RegisterBox/RegisterBox';
 function App() {
   const [loginScreen, setLoginScreen] = useState(true);
 
+  const exampleSocket = new WebSocket('ws://stream.tradingeconomics.com/?client=guest:guest');
+
+  exampleSocket.onopen = function () {
+    exampleSocket.send({
+      "topic": "subscribe",
+      "to": "EURUSD:CUR"
+    });
+    console.log('socket send')
+  };
+
+  exampleSocket.onmessage = function(data) {
+    console.log(data)
+  }
+
+  console.log(exampleSocket)
+
   return (
     <div className="App">
       <button className='screen-button' onClick={() => setLoginScreen(!loginScreen)}>{loginScreen ? 'Register' : 'Log In'}</button>
